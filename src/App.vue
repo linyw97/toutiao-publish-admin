f63aca78f229510485051f5439c4ff56081cb104
linyw97 <932496546@qq.com>
2020-11-10 15:17:58 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <!-- <router-link to="/">Home</router-link> | -->
-      <!-- <router-link to="/about">About</router-link> -->
-    </div>
+    <h3>头条内容发布管理系统</h3>
     <!-- 路由出口 -->
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App'
+}
+</script>
 <style lang="less">
-
 </style>
