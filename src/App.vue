fd510b9b392cccc4a2fa380160cf2ba197198f10
linyw97 <932496546@qq.com>
2020-11-10 14:25:58 +0800

    目录结构调整

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <!-- <router-link to="/">Home</router-link> | -->
+      <!-- <router-link to="/about">About</router-link> -->
+    </div>
+    <!-- 路由出口 -->
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
